2d85b48650236cd73c86b4f324f61890cfd385c6
Mariana Sarovic <sarovicmariana@gmail.com>
2023-06-14 21:06:23 +0200

    Opravene menu - funguje

--- src/pages/navbar.jsx
+++ src/pages/navbar.jsx
@@ -8,26 +8,26 @@ export const Navbar = () => {
 
   const toggleMenu = () => setShowLinks(!showLinks);
 
-  const renderMenuLinks = () => {
-    if (showLinks) {
-      return (
-        <div className="menu menu__hyperlinks">
-          <div className="menu__pages">
-            <Link className="a a__pages" to="/intro">
-              {lang.menu.intro}
-            </Link>
-            <Link className="a a__pages" to="/AboutUs">
-              {lang.menu.aboutus}
-            </Link>
-            <Link className="a a__pages" to="/contact">
-              {lang.menu.contact}
-            </Link>
-          </div>
-        </div>
-      );
-    }
-    return null;
-  };
+  // const renderMenuLinks = () => {
+  //   if (showLinks) {
+  //     return (
+  //       <div className="menu menu__hyperlinks">
+  //         <div className="menu__pages menu__pages--mobile">
+  //           <Link className="a a__pages" to="/intro">
+  //             {lang.menu.intro}
+  //           </Link>
+  //           <Link className="a a__pages" to="/AboutUs">
+  //             {lang.menu.aboutus}
+  //           </Link>
+  //           <Link className="a a__pages" to="/contact">
+  //             {lang.menu.contact}
+  //           </Link>
+  //         </div>
+  //       </div>
+  //     );
+  //   }
+  //   return null;
+  // };
 
   return (
     <>
@@ -38,7 +38,21 @@ export const Navbar = () => {
           <FaBars style={{ fontSize: '24px', color: 'var(--text-color)' }} />
         )}
       </div>
-      {renderMenuLinks()}
+      <div
+        className={`menu menu__hyperlinks ${
+          showLinks ? 'menu__pages--mobile' : 'menu__pages'
+        }`}
+      >
+        <Link className="a a__pages" to="/intro">
+          {lang.menu.intro}
+        </Link>
+        <Link className="a a__pages" to="/AboutUs">
+          {lang.menu.aboutus}
+        </Link>
+        <Link className="a a__pages" to="/contact">
+          {lang.menu.contact}
+        </Link>
+      </div>
     </>
   );
 };
