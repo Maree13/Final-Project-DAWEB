3ae37634104ab12b4fc94413ddd37d701b40006a
Mariana Sarovic <sarovicmariana@gmail.com>
2023-06-16 15:11:42 +0200

    {

--- src/pages/aboutus.jsx
+++ src/pages/aboutus.jsx
@@ -5,19 +5,21 @@ export const AboutUs = () => {
   return (
     <main className="main__aboutus">
       <div className="figure__aboutus figure__aboutus--vira">
-        <figure className="figure__foto--mari">
+        <figure className="figure__foto--vira">
           <img
             className="img__profile img__profile--vira"
             src="img/Vira.jpg"
             alt="Vira profile"
           />
-          <h1>Vira</h1>
-          <p className="paragraph">
-            Mám ráda život a hory. V každé situaci se snažím najít pozitiv. A
-            hlavním důvodem, proč se já chci přidat do IT, jsou lidé. Chci
-            pracovat, studovat a komunikovat s lidmi, kteří jsou otevření všemu:
-            novým změnám, výzvám, známostem, pohybu.
-          </p>
+          <div class="figure__aboutus--textpart">
+            <h1 class="figure__profile--title">Vira</h1>
+            <p className="figure__profile--paragraph">
+              Mám ráda život a hory. V každé situaci se snažím najít pozitiv. A
+              hlavním důvodem, proč se já chci přidat do IT, jsou lidé. Chci
+              pracovat, studovat a komunikovat s lidmi, kteří jsou otevření
+              všemu: novým změnám, výzvám, známostem, pohybu.
+            </p>
+          </div>
         </figure>
       </div>
       <div className="figure__aboutus figure__aboutus--mari">
@@ -27,15 +29,16 @@ export const AboutUs = () => {
             src="img/Mari.JPG"
             alt="Mari profile"
           />
-          <h1>Mari</h1>
-          <p className="paragraph">
-            Som slovensko-čiernohorského pôvodu, ale bývam v Prahe, kde sa cítim
-            ako doma. Vždy som bola skôr humanitne zameraná, a tak je pre mňa
-            programovanie veľkou výzvou. Web som si vybrala preto, lebo spája v
-            sebe logiku a estetično. Rada by som sa podielala na projektoch,
-            ktoré maju presah. Najšťastnejšia som na samote v horách a milujem
-            kávu.
-          </p>
+          <div class="figure__aboutus--textpart">
+            <h1 class="figure__profile--title">Mari</h1>
+            <p className="figure__profile--paragraph">
+              Na svojej ceste za vzdelanim som bola vždy skôr humanitne
+              zameraná, a tak je pre mňa programovanie veľkou výzvou. Web som si
+              vybrala preto, lebo v ňom môžem skĺbiť logiku a design. Rada sa
+              podieľam na projektoch, ktoré maju presah a môžu ľuďom pomôcť.
+              Najšťastnejšia som na samote v horách a milujem kávu.
+            </p>
+          </div>
         </figure>
       </div>
     </main>
